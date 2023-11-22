808748fdcf4d9c32b00c028ee37cdf4d1cfca7a0
GerardSiurana <144882209+GerardSiurana@users.noreply.github.com>
2023-11-22 08:30:17 +0100

    Add files via upload

---
+++ app-vitaminada/Controller/AdminController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    #[Route('/admin/images', name: 'app_images')]
+    public function images(): Response
+    {
+        return $this->render('admin/images.html.twig', []);
+    }
+
+    public function adminDashboard(): Response
+    {   
+    $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+    // or add an optional message - seen by developers
+    $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+    
+    new Response("Sí que puedes entrar");
+    }
+
+    #[Route('/admin/categories', name: 'app_categories')]
+    public function categories(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $repositorio = $doctrine->getRepository(Category::class);
+
+        $categories = $repositorio->findAll();
+
+        $category = new Category();
+        $form = $this->createForm(CategoryFormType::class, $category);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $category = $form->getData();    
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($category);
+            $entityManager->flush();
+        }
+        return $this->render('admin/categories.html.twig', array(
+            'form' => $form->createView(),
+            'categories' => $categories   
+        ));
+
+    }
+
+
+
+}
